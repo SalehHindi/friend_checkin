fddadc139d35cfa096c62f65c222551724509d7b
Saleh Hindi <saleh.hindi.one+git@gmail.com>
2024-04-20 15:57:19 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home.js';
-import Transcripts from './components/Transcripts.js';
-import Transcript from './components/Transcript.js';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/transcripts" element={<Transcripts />} />
-        <Route path="/transcript/:id" element={<Transcript />} />
+        {/* <Route path="/home" element={<Home />} /> */}
       </Routes>
     </Router>
   );
--- src/components/Transcript.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Transcript = () => {
-  return <h1>Transcript Details</h1>;
-};
-
-export default Transcript;
